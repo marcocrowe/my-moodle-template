2ab978c08d640708f3f2d192d711108c948c40b3
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-04-19 08:01:28 +0100

    update code

---
+++ update.py
@@ -0,0 +1,18 @@
+"""
+Copyright © 2024 Mark Crowe <https://github.com/marcocrowe>. All rights reserved.
+Update template
+"""
+
+from os import getcwd
+from my_moodle import update_my_moodle_template
+
+CURRENT_VERSION = "1.0.3"
+
+
+def main():
+    """Update the My Moodle template."""
+    update_my_moodle_template(getcwd(), CURRENT_VERSION)
+
+
+if __name__ == "__main__":
+    main()
